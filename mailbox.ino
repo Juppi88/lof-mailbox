73c71a1e94778a82df1ae4d33d3dcbd89808ff84
Tuomo Jauhiainen <tuomo.jauhiainen@wapice.com>
2018-04-19 18:03:47 +0300

    Interrupt for the door switch.

--- mailbox.ino
+++ mailbox.ino
@@ -2,34 +2,67 @@
 #include <VL6180X.h>
 #include <NewPing.h>
 
+const int SWITCH_PIN = 7;
+
+static void blink(int times);
+static void on_box_opened(void);
+
 VL6180X sensor;
 NewPing sonar(5, 4, 200);
+
+////////////////////////////////////////
 
 void setup()
 {
   // Initialize the distance sensor.
   Wire.begin();
-  
   sensor.init();
   sensor.configureDefault();
   sensor.setTimeout(500);
 
   // Enable built-in LED for debugging.
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
+  // Start serial interface.
   Serial.begin(57600);
   while (!Serial) { }
+
+  // Attach an interrupt to the switch pin.
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), on_box_opened, FALLING);
 }
+
+////////////////////////////////////////
 
 void loop()
 {
-  uint16_t distance = sensor.readRangeSingleMillimeters();
+  uint16_t distance;
 
-  digitalWrite(LED_BUILTIN, distance < 100 ? HIGH : LOW);
+  // Read distance from the ultrasonic sensor.
+  distance = distance = sonar.convert_cm(sonar.ping_median(5));
 
-  int distance2 = sonar.convert_cm(sonar.ping_median(5));
-  Serial.println(distance2);
+   // Read distance from the other sensor.
+  distance = sensor.readRangeSingleMillimeters();
   
-  delay(100);
+  //Serial.println(distance);
 }
+
+////////////////////////////////////////
+
+static void blink(int times)
+{
+  for (int i = 0; i < times; i++) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(250);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(250);
+  }
+}
+
+static void on_box_opened(void)
+{
+  Serial.println("Interruptaannun täällä nyt");
+}
+
 
